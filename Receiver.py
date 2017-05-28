baf96d4a93a445fe63610e5b6df87689703d2842
JasonAnx <jasonanchia@hotmail.com>
2017-05-28 16:54:39 -0600

    sending and receiving without order

--- Receiver.py
+++ Receiver.py
@@ -1,18 +1,82 @@
 import socket
+import struct, sys
 
 UDP_IP = "127.0.0.1"
 UDP_PORT = 5005
 
-sock = socket.socket(socket.AF_INET, # Internet
-                     socket.SOCK_DGRAM) # UDP
-sock.bind((UDP_IP, UDP_PORT))
+"asdfasfsadfasdf"
+class Server(object):
+    "asfddsfdf"
 
-sock.settimeout(30)
+    def __init__(self):
+        # Crea la conexion
+        self.sock = socket.socket(socket.AF_INET,    # Internet
+                                  socket.SOCK_DGRAM)  # UDP
+        my_dir = (UDP_IP, UDP_PORT)
+        self.sock.bind(my_dir)
+        self.my_window_size = 500
+        self.file = 0
+        print("listening on", my_dir)
+
+    def receive ():
+        "asdfasdfasdf"
+        # while True:
+        return 0
+
+    def start(self):
+        self.negotite_window()
+        self.file = open("Output.txt", "w")
+        self.receive_segments()
+        self.file.close()
 
 
-# while True:
-#     data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
-#     print ("received message:", data)
-    
-    
-    
+    def negotite_window(self):
+        "negotite window"
+        print("waiting for connection")
+        data, addr = self.sock.recvfrom(100) # buffer size in bytes
+
+        unpacked = struct.unpack("ii11s", data) # https://docs.python.org/3/library/struct.html
+        window_size_rcv, port, ip = unpacked
+        ipcdd = ip.decode("utf-8")
+        print("A connection from :", ipcdd,"port", port, "has been requested. It declares a windows size of:", window_size_rcv, "bytes")
+        print("informing client that our max windows size is", self.my_window_size)
+        if ( window_size_rcv < self.my_window_size ):
+            print( "received window size is smaller than our window size" )
+            self.my_window_size = window_size_rcv
+
+
+        # sending syn-ack
+        TARGET_IP =  str.encode(ipcdd.translate( dict.fromkeys(range(32)) )) # removing garbage from Sender ip and casting to bytes
+        TARGET_PORT = port
+        my_ip = str.encode(UDP_IP)
+        data = struct.pack("ii11s", self.my_window_size, UDP_PORT, my_ip)
+        self.sock.sendto(data, (TARGET_IP, TARGET_PORT))
+
+        # wait ack
+        data, addr = self.sock.recvfrom(100) # buffer size in bytes
+        unpacked = struct.unpack("ii", data) # https://docs.python.org/3/library/struct.html
+        ack, seq_num = unpacked
+        print(ack, seq_num)
+        return True
+
+        # self.sock.sendto(str.encode('4096'), (UDP_IP, UDP_PORT))
+        # tcp_header = struct.pack("!hH16s", seq_num, flags, digested)
+
+    # sock.close()
+    def receive_segments(self):
+        print( "windows size of ",  self.my_window_size, "bytes" )
+        while True:
+            data  = self.sock.recv(    self.my_window_size ) # buffer size in bytes
+            headersize = 73
+            bodysize = self.my_window_size - headersize
+            unpacked = struct.unpack("11si11siii%ds"%bodysize, data) # https://docs.python.org/3/library/struct.html
+
+            source_ip,     source_port,     destination_ip,      destination_port,    seq_num,     ack, data = unpacked
+            # print (unpacked)
+            self.file.write(    data.decode("utf-8")    )
+        # print ( sys.getsizeof( header ) )
+        return
+
+
+S = Server()
+S.start()
--- Sender.py
+++ Sender.py
@@ -6,39 +6,124 @@
 """
 
 import socket
-import sys # used to read the file to send
+import sys  # used to read the file to send
+import struct
+import math
 
-# Variables --------------------------------------------------------------------------------
+# Variables --------------------------------------------------------------
+
+TARGET_IP = "127.0.0.1"
+TARGET_PORT = 5005
+
+print("UDP target IP:", TARGET_IP)
+print("UDP target port:", TARGET_PORT)
 
 UDP_IP = "127.0.0.1"
-UDP_PORT = 5005
-data = "" # we will store here the data to be sent
+UDP_PORT = 5006
 
-# EO Variables -----------------------------------------------------------------------------
+class Sender(object):
+    "sadfdafsfasdfasdadf"
 
+    def __init__(self):
+        self.data = ""  # we will store here the data to be sent
+        # Crea la conexion
+        self.sock = socket.socket(socket.AF_INET,    # Internet
+                                  socket.SOCK_DGRAM)  # UDP
+        my_dir = (UDP_IP, UDP_PORT)
+        self.sock.bind(my_dir)
+        self.window_size = 4096
+        print("listening on", my_dir)
 
-print ("UDP target IP:", UDP_IP)
-print ("UDP target port:", UDP_PORT)
+    header_size = 20
+    mss = 1480 - header_size  # > https://en.wikipedia.org/wiki/Maximum_segment_size
+    # MSS specifies the largest amount of data, specified in bytes, that a computer
+    # or communications device can receive in a single TCP segment"""
 
-#Crea la conexion
-sock = socket.socket(socket.AF_INET,    # Internet
-                     socket.SOCK_DGRAM) # UDP
-                     
-# Methods --------------------------------------------------------------------------------
-def read_input():
-    if len(sys.argv) != 2:
-        print('[Error]:\n\tNo file or more than 1 file specified\n')
-        return
-    with open(sys.argv[1], 'r') as my_file:
-        data = str.encode(    my_file.read()  )
-
-
-    # send()
-# EO Methods -----------------------------------------------------------------------------
+    # EO Variables -----------------------------------------------------------
 
 
-read_input()
+    # Methods ----------------------------------------------------------------
+
+    def read_input_file(self):
+        "asfsdafasadf"
+        if len(sys.argv) != 2:
+            print('[Error]:\n\tNo file or more than 1 file specified\n')
+            exit(1)
+        with open(sys.argv[1], 'r') as my_file:
+            file = my_file.read()
+            self.data = str.encode(file)
+
+            # print( sys.getsizeof(  file ) )
+            size_in_bytes = sys.getsizeof(self.data)
+            print("transmitting", size_in_bytes, type(self.data).__name__, "(",
+                  size_in_bytes / 1000, "kb)", "of data"
+                 )
+
+        # send()
+
+    def start(self):
+        "start negotiting the window size"
+        # send syn
+
+        self.read_input_file()
+        ipcdd = str.encode(UDP_IP)
+        data = struct.pack("ii11s", self.window_size, UDP_PORT, ipcdd) # "https://docs.python.org/3/library/struct.html"
+        self.sock.sendto(data, (TARGET_IP, TARGET_PORT))
+
+        # wait for syn-ack
+        data, addr = self.sock.recvfrom(100) # buffer size in bytes
+
+        unpacked = struct.unpack("ii11s", data) # https://docs.python.org/3/library/struct.html
+        window_size_svr, port, ip = unpacked
+        ipcdd = ip.decode("utf-8")
+        print("A connection from :", ipcdd,"port", port, "has been requested. It requests a windows size of:", window_size_svr, "bytes")
+        self.window_size = window_size_svr
+
+        # send ack
+        data = struct.pack("ii", 0, 0)
+        self.sock.sendto(data, (TARGET_IP, TARGET_PORT))
+
+    def sendSeg(self):
+        "send segments of the message to the receiver"
+
+        # window_size - headersize
+        n = self.window_size - 73
+        sgmts = [self.data[i:i+n] for i in range(0, len(self.data), n )]
+
+        numbsegments = len(sgmts)
+        print( "Prepairing to send", numbsegments, "segments" )
+        print( "size (in bytes) of each segment is ", sys.getsizeof( sgmts[0] ) )
+
+        # print(sgmts[0])
+
+        source_ip = str.encode(UDP_IP)
+        destination_ip = str.encode(TARGET_IP)
+        ack = math.ceil(sys.getsizeof(self.data)/self.window_size)
+        seq_num = 1
+
+        segment_format = "%ds"%len(sgmts[0])
+        for idx in range( numbsegments ): # python 3 range is the same as python2 xrange
+            print("sending segment %d"%idx)
+            # Header:
+            # source_ip     source_port     destination_ip      destination_port    seq_num     ack
+            header = struct.pack ("11si11siii",source_ip,UDP_PORT, destination_ip, TARGET_PORT, seq_num, ack)
+            headersize = sys.getsizeof( header )
+            if ( headersize != 73 ):
+                print("header size mismatch")
+                exit(23)
+            sgdata = struct.pack ( segment_format, sgmts[idx]  ) # pack the segment data
+            segmnt = header + sgdata
+            # print ( segmnt )
+            # print ( sys.getsizeof( segmnt ) )
+            self.sock.sendto(segmnt, (TARGET_IP, TARGET_PORT))
+
+    # EO Methods -------------------------------------------------------------
+
+
+SNDR = Sender()
+SNDR.start()
+SNDR.sendSeg()
 sys.exit(0)
 
-                     
+
 # sock.sendto(MESSAGE_as_bytes, (UDP_IP, UDP_PORT))
